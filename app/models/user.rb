938b7b8cf4c97b0317a8daa871e1218497ed4210
ana-wtag <najia.afrin@welldev.io>
2023-07-05 14:13:01 +0600

    add: devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  enum :role, { admin: 0, customer: 1 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmls
-  Rails.application.routes.draw do
   devise_for :users
     # root to: defines which controller action will handle requests to the root path
     #in this case, the route will be http://localhost:3000
     # The requests to this route will be handled by the index action in the home controller
     root to: "home#index" 
   end
-end
